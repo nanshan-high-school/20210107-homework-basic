cdd586c0661af02b84ec19f29e0d068e4f5cfdee
papayaisnotafood <70697738+papayaisnotafood@users.noreply.github.com>
2021-01-14 13:02:18 +0800

    Update transcript.cpp

--- 755001/transcript.cpp
+++ 755001/transcript.cpp
@@ -7,19 +7,19 @@ int main() {
     vector<int> vec;
     int inputnum;
     float avegfront, avegback;
-    while(cin >> inputnum) {
-        if(inputnum != 0) {
+    while (cin >> inputnum) {
+        if (inputnum != 0) {
             vec.push_back(inputnum);
             inputnum = 0;
         }else {
             sort(vec.begin(), vec.end());
-            for(int n = 0; n < vec.size(); n++) {
-                if(n < (vec.size() / 2) - 1) {
+            for (int n = 0; n < vec.size(); n++) {
+                if (n < (vec.size() / 2) - 1) {
                     avegfront = avegfront + vec.at(n);
-                }else if(n == (vec.size() / 2) - 1) {
+                }else if (n == (vec.size() / 2) - 1) {
                     avegfront = avegfront + vec.at(n);
                     cout << "前半:" << avegfront / (vec.size()/2) << endl;
-                }else if(n == vec.size() - 1) {
+                }else if (n == vec.size() - 1) {
                     avegback = avegback + vec.at(n);
                     cout << "後半:" << avegback / (vec.size()/2) << endl;
                 }else {
